c011205ed265d85ae45ce4e6bef186a67833d6af
Neil Young <46945214+youngn46@users.noreply.github.com>
2019-04-17 09:57:01 -0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,42 +1,12 @@
-//delete
-
-
-/***********************************************
-* Name: Takeshwari Kamal
-* Email: kamalt4@students.rowan.edu
-***********************************************
-* Purpose:
-*  This program calculates area of circle
-***********************************************/
+/***********************************************************************************
+* Name:		Second Row Final Project
+* Members:	Alex B., Alex M., Issolif K., Kristen H., Neil Y., Nick S., & Rob M.
+* Purpose:	Airline Booking & Admin Program
+***********************************************************************************/
 
 #include <iostream>
-
 using namespace std; 
 
-const  float PI = 3.14159;
-
-int main()
-{
-
-	float radius, area, circumference;
+int main(){
 	
-	//Display purpose of the program
-	cout << " ******************************************"<<endl;
-	cout << " This program takes in radius to compute area of circle" <<endl;
-	cout << " ******************************************\n\n";
-	
-	//Get user input
-	cout << "Please input radius: ";
-	cin >> radius;
-	
-	//Compute area to PI*radius*radius
-	area = PI* radius *radius; 
-	
-	//Display the area 
-	cout << " The area is = " << area << "\n\n";
-	cout << "Program has ended"<<endl;
-
-	system("pause");
-	
-	return 0;
 }
