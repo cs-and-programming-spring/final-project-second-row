cc6ecf0aa3f462400a334d791af39adb9577918c
Neil Young <46945214+youngn46@users.noreply.github.com>
2019-04-25 19:26:05 -0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,6 @@
 * Purpose:	Wedding Planning Program
 *************************************************************************************/
 
-
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -13,67 +12,36 @@ using namespace std;
 void newBooking();
 
 int main() {
+	
+//Create User Struct
+struct guest {
+	string name;
+	bool plusOne;
+	enum gift {China, Silverware, Trip, Cash};
+	enum meal {Steak, Chicken, Vegetarian};
+	bool hotel;
+	string ccnum;
+};
+	
+	cout << "Welcome to the Wedding RSVP Program." << endl;
+	
+//RSVP
 
-	//--Program Menu--
-	cout << "-----Wedding Planning Menu-----" << endl;
-	bool run = true;
-	int choice = 0;
-	while (run) {
-		cout << "Available programs:" << endl;
-		cout << "(1) Program 1" << endl;
-		cout << "(2) Program 2" << endl;
-		cout << "(3) Program 3" << endl;
-		cout << "(4) Program 4" << endl;
-		cout << "(5) Program 5" << endl;
-		cout << "(6) Program 6" << endl;
-		cout << "(7) Book Hotel Room" << endl;
-		cout << "Please choose a program to run from the list above: ";
-		cin >> choice;
-		//validate choice with while-loop
-		while (choice < 1 || choice > 7) {
-			cout << "Please enter a number between 1 & 7: ";
-			cin >> choice;
-		} //while
-		cout << "\n";
+	cout << "-----RSVP-----" << endl;
+	//INSERT RSVP CODE
 
-		//--Program 1--
-		if (choice == 1) {
-			cout << "-----Program 1-----" << endl;
-			//INSERT CODE FOR PROGRAM 1
-		} //1
+//GIFT REGISTRY
 
-		//--Program 2--
-		if (choice == 2) {
-			cout << "-----Program 2-----" << endl;
-			//INSERT CODE FOR PROGRAM 2
-		} //2
+	cout << "-----Gift Registry-----" << endl;
+	//INSERT GIFT CODE
 
-		//--Program 3--
-		if (choice == 3) {
-			cout << "-----Program 3-----" << endl;
-			//INSERT CODE FOR PROGRAM 3
-		} //3
+//MEAL CHOICE
 
-		//--Program 4--
-		if (choice == 4) {
-			cout << "-----Program 4-----" << endl;
-			//INSERT CODE FOR PROGRAM 4
-		} //4
+	cout << "-----Meal Choice-----" << endl;
+	//INSERT MEAL CODE
 
-		//--Program 5--
-		if (choice == 5) {
-			cout << "-----Program 5-----" << endl;
-			//INSERT CODE FOR PROGRAM 5
-		} //5
 
-		//--Program 6--
-		if (choice == 6) {
-			cout << "-----Program 6-----" << endl;
-			//INSERT CODE FOR PROGRAM 6
-		} //6
-
-		//--Program 7--
-		if (choice == 7) {
+//HOTEL BOOKING
 			cout << "-----Hotel Booking-----" << endl;
 			bool reg = true;
 			string book;
@@ -94,17 +62,6 @@ int main() {
 	
 				if (reg == false)
 					cout << "OK. We hope you enjoy the Wedding!" << endl;
-			} //7
-
-		//--Run Another Program?
-		string answer;
-		cout << endl << "Would you like to run another program? (y/n) ";
-		cin >> answer;
-		cout << "\n";
-		if (answer == "No" || answer == "no" || answer == "NO" || answer == "n" || answer == "N")
-			run = false;
-
-	} //run while
 
 	cout << endl << "The End. Goodbye.";
 	system("pause");
